728fd6a6c640010824870cb1ab0e63883bb1bf79
y-shimada <yuto2sleepy@gmail.com>
2022-03-29 07:36:41 +0900

    追加　コメント文 追加　=,+,*の前後のスペース 変更　信号の生成のセクション化 変更　変数名の頭文字 大文字→小文字

--- stftPracticeMain.m
+++ stftPracticeMain.m
@@ -1,21 +1,21 @@
 % 5年生の短時間Fourier変換（short-time Fourier transform: STFT）実習
 clear; close all; clc;
 
-% 440Hz＋880Hzの信号（10秒，サンプリング周波数16kHz）を生成
-time = 10;
-Fs = 16000;
-f1=440;
-f2=880;
-omega1=2*pi*f1;
-omega2=2*pi*f2;
-t=0:Fs:time;
+%% 440Hz＋880Hzの信号（10秒，サンプリング周波数16kHz）を生成
+time = 10;  %信号の時間長
+Fs = 16000; %サンプリング周波数
+f1 = 440;   %1つめの信号の周波数
+f2 = 880;   %2つめの信号の周波数
+omega1 = 2 * pi * f1;   %1つめの信号の角周波数
+omega2 = 2 * pi * f2;   %2つめの信号の角周波数
+t = 0:1/Fs:time;    %変数t
 
-Wave1=sin(omega1*t);
-Wave2=sin(omega2*t);
+wave1 = sin(omega1 * t);    %1つめの信号
+wave2 = sin(omega2 * t);    %2つめの信号
 
-Wave=Wave1+Wave2;
+wave = wave1 + wave2;
 
-% STFT実装
+%% STFT実装
 
 % パワースペクトログラムの表示
 
